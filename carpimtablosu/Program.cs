6e42b81a86d1c4416dbad2c828f01f67c5c4be81
Beyzaktas <134061187+Beyzakttas@users.noreply.github.com>
2024-02-03 17:00:37 +0300

    Add project files.

---
+++ carpimtablosu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carpimtablosu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1;i<=10; i++)// 100
+            {
+                for(int j = 1; j <= 10; j++)//1000kere calisir
+                {
+                    Console.Write(i  +   "x"  + j+ "="  + ( i  *  j) );
+                    Console.Write("     ");
+                }
+                Console.WriteLine("");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ login uygulaması/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace login_uygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanici giris uyg 
+            //adi ve sifresi dogru ise tebrikler 
+            //yanlis ise hata ver 3 hak ver 
+            int haksayisi = 3;
+            while (true)
+                
+            {
+                Console.WriteLine("kullanici adinizi girin");
+                string kullaniciadi = Console.ReadLine();
+                Console.WriteLine("sifrenizi girin");
+                string sifre = Console.ReadLine();
+                if (kullaniciadi=="beyza" && sifre == "123")
+                {
+                    Console.WriteLine("tebrikler giris yaptiniz");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("kullanici adi veya sifre yanlis");
+                }
+                if (haksayisi > 0)
+                {
+                    haksayisi -= 1;
+                    Console.WriteLine("kalan hak sayısı:" + haksayisi);
+                }
+                if (haksayisi ==0)
+                {
+                    Console.WriteLine("hakkiniz kalmadi");
+                    break;
+                }
+                
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
